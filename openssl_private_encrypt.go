4f6644a94cd901399e4f87c0aba91da06d9c0079
Ali-A-A <aliabbasi806@gmail.com>
2022-02-14 11:26:58 -0800

    Feat encryption function

--- openssl_private_encrypt.go
+++ openssl_private_encrypt.go
@@ -1,1 +1,40 @@
 package openssl_private_encrypt
+
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+)
+
+const (
+	// RSAPrivateType is type that taken from the preamble
+	RSAPrivateType = "RSA PRIVATE KEY"
+)
+
+// OpensslPrivateEncrypt encrypt data using privateKey.
+// privateKey should be PKCS1.
+func OpensslPrivateEncrypt(data, privateKey string) (string, bool) {
+	block, _ := pem.Decode([]byte(privateKey))
+	if block == nil {
+		return "", false
+	}
+
+	if block.Type != RSAPrivateType {
+		return "", false
+	}
+
+	rsaPrivateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return "", false
+	}
+
+	s, err := rsa.SignPKCS1v15(rand.Reader, rsaPrivateKey, crypto.Hash(0), []byte(data))
+	if err != nil {
+		return "", false
+	}
+
+	return base64.StdEncoding.EncodeToString(s), true
+}
--- openssl_private_encrypt_test.go
+++ openssl_private_encrypt_test.go
@@ -1,1 +1,84 @@
-package openssl_private_encrypt
+package openssl_private_encrypt_test
+
+import (
+	ope "github.com/Ali-A-A/openssl-private-encrypt"
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+func testingKey(s string) string { return strings.ReplaceAll(s, "TESTING KEY", "PRIVATE KEY") }
+
+var pemPrivateKey = testingKey(`-----BEGIN RSA TESTING KEY-----
+MIIBOgIBAAJBALKZD0nEffqM1ACuak0bijtqE2QrI/KLADv7l3kK3ppMyCuLKoF0
+fd7Ai2KW5ToIwzFofvJcS/STa6HA5gQenRUCAwEAAQJBAIq9amn00aS0h/CrjXqu
+/ThglAXJmZhOMPVn4eiu7/ROixi9sex436MaVeMqSNf7Ex9a8fRNfWss7Sqd9eWu
+RTUCIQDasvGASLqmjeffBNLTXV2A5g4t+kLVCpsEIZAycV5GswIhANEPLmax0ME/
+EO+ZJ79TJKN5yiGBRsv5yvx5UiHxajEXAiAhAol5N4EUyq6I9w1rYdhPMGpLfk7A
+IU2snfRJ6Nq2CQIgFrPsWRCkV+gOYcajD17rEqmuLrdIRexpg8N1DOSXoJ8CIGlS
+tAboUGBxTDq3ZroNism3DaMIbKPyYrAqhKov1h5V
+-----END RSA TESTING KEY-----
+`)
+
+var invalidTypeKey = testingKey(`-----BEGIN TESTING KEY-----
+MIIBOgIBAAJBALKZD0nEffqM1ACuak0bijtqE2QrI/KLADv7l3kK3ppMyCuLKoF0
+fd7Ai2KW5ToIwzFofvJcS/STa6HA5gQenRUCAwEAAQJBAIq9amn00aS0h/CrjXqu
+/ThglAXJmZhOMPVn4eiu7/ROixi9sex436MaVeMqSNf7Ex9a8fRNfWss7Sqd9eWu
+RTUCIQDasvGASLqmjeffBNLTXV2A5g4t+kLVCpsEIZAycV5GswIhANEPLmax0ME/
+EO+ZJ79TJKN5yiGBRsv5yvx5UiHxajEXAiAhAol5N4EUyq6I9w1rYdhPMGpLfk7A
+IU2snfRJ6Nq2CQIgFrPsWRCkV+gOYcajD17rEqmuLrdIRexpg8N1DOSXoJ8CIGlS
+tAboUGBxTDq3ZroNism3DaMIbKPyYrAqhKov1h5V
+-----END TESTING KEY-----
+`)
+
+var invalidBlockKey = testingKey(`-----BEGIN RSA TESTING KEY-----
+MIIBOgIBAAJBALKZD0nEffqM1ACuak0bijtqE2QrI/KLADv7l3kK3ppMyCuLKoF0
+-----END RSA TESTING KEY-----
+`)
+
+func TestOpensslPrivateEncrypt(t *testing.T) {
+	cases := []struct {
+		name       string
+		data       string
+		privateKey string
+		shouldFail bool
+	}{
+		{
+			name:       "successful",
+			privateKey: pemPrivateKey,
+			data:       "ali",
+			shouldFail: false,
+		},
+		{
+			name:       "invalid type",
+			privateKey: invalidTypeKey,
+			data:       "ali",
+			shouldFail: true,
+		},
+		{
+			name:       "invalid block",
+			privateKey: invalidBlockKey,
+			data:       "ali",
+			shouldFail: true,
+		},
+		{
+			name:       "empty data",
+			privateKey: pemPrivateKey,
+			data:       "",
+			shouldFail: false,
+		},
+	}
+
+	for _, tt := range cases {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			encryptedData, ok := ope.OpensslPrivateEncrypt(tt.data, tt.privateKey)
+			if tt.shouldFail {
+				assert.Equal(t, false, ok)
+			} else {
+				assert.Equal(t, true, ok)
+				assert.NotEqual(t, 0, len(encryptedData))
+			}
+		})
+	}
+}
